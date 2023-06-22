354536602d870e63636bea15f7bfd2449146d674
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-22 10:25:35 +0200

    Add files via upload

---
+++ src/MegaWorld/Main.php
@@ -0,0 +1,21 @@
+<?php
+namespace MegaWorld;
+
+use MegaWorld\Commands\MegaWorldCommand;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class Main extends PluginBase{
+    use SingletonTrait;
+
+    public function onLoad(): void{
+        self::setInstance($this);
+    }
+
+    public function onEnable(): void{
+        Server::getInstance()->getCommandMap()->registerAll($this->getName(), $this[
+            new MegaWorldCommand()
+        ]);
+    }
+}
