1abb6e35455d71d2b6d601e9c8fbb9eb9e2f6bd8
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-23 10:46:05 +0200

    Update Main.php

--- src/MegaWorld/Main.php
+++ src/MegaWorld/Main.php
@@ -24,7 +24,7 @@ class Main extends PluginBase{
                                 if(empty($args[2])){
                                     $sender->sendMessage("§cPlease enter a Generator name!");
                                 }else{
-                                    Server::getInstance()->getWorldManager()->generateWorld($args([1]), $args([2]);
+                                    Server::getInstance()->getWorldManager()->generateWorld($args([1]), $args([2]));
                                     $sender->sendMessage("§aSuccessfully Generated the World " . $args([1]) . " §aWith the Generator: " . $args([2]));
                                 }
                                 break;
